05e24a430c9b0552d0fcd795f880c4748d7a39d3
Olesia Tsaturian <olesiases@gmail.com>
2026-01-18 19:54:26 +0200

    change form details

---
+++ app/notes/action/create/page.tsx
@@ -0,0 +1,24 @@
+import { Metadata } from 'next';
+import CreateNote from './CreateNote.client';
+
+export const metadata: Metadata = {
+  title: 'Create note',
+  description: 'Create a new note',
+  openGraph: {
+    title: 'Create note',
+    description: 'Create a new note',
+    url: 'https://notehub.app',
+    images: [
+      {
+        url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'NoteHub',
+      },
+    ],
+  },
+};
+
+export default function CreateNotePage() {
+  return <CreateNote />;
+}
--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -1,103 +1,253 @@
+'use client';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import css from './NoteForm.module.css';
-import { Formik, Form, Field, type FormikHelpers, ErrorMessage } from 'formik';
+
 import * as Yup from 'yup';
-import toast from 'react-hot-toast';
+import toast, { Toaster } from 'react-hot-toast';
 import { createNote, type CreateParams } from '../../lib/api';
+import { useRouter } from 'next/navigation';
+import { useNoteDraftStore } from '@/lib/stores/noteStore';
 
-interface NoteFormProps {
-  onClose: () => void;
-}
-
-const newValidationSchema = Yup.object({
-  title: Yup.string()
-    .min(3, 'Title must be at least 3 characters')
-    .max(50, 'Title must be not longer than 50 characters')
-    .required('Title is required')
-    .trim(),
-  content: Yup.string()
-    .max(500, 'Content must be at most 500 characters')
-    .trim(),
-  tag: Yup.string()
-    .oneOf(['Todo', 'Work', 'Personal', 'Meeting', 'Shopping'])
-    .required('Please select a tag'),
-});
-
-export default function NoteForm({ onClose }: NoteFormProps) {
+export default function NoteForm() {
+  const { draft, setDraft, clearDraft } = useNoteDraftStore();
   const queryClient = useQueryClient();
+  const router = useRouter();
 
-  const noteMutation = useMutation({
+  const validationSchema = Yup.object({
+    title: Yup.string()
+      .min(3, 'Title must be at least 3 characters')
+      .max(50, 'Title must be at most 50 characters')
+      .required('Title is required')
+      .trim(),
+    content: Yup.string()
+      .max(500, 'Content must be at most 500 characters')
+      .trim(),
+    tag: Yup.string()
+      .oneOf(
+        ['Todo', 'Work', 'Personal', 'Meeting', 'Shopping'],
+        'Please select a tag',
+      )
+      .required('Please select a tag'),
+  });
+
+  const createNoteMutation = useMutation({
     mutationFn: (newNote: CreateParams) => createNote(newNote),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['notes'] });
-      onClose();
-      toast.success('Note added successfully!');
+
+      toast.success('Note added successfully!', { duration: 7000 });
+
+      clearDraft();
+
+      router.back();
     },
     onError: () => {
-      toast.error('Failed to add note');
+      toast.error('Failed to add note. Please try again.', { duration: 7000 });
     },
   });
-  const formValues = (
-    values: CreateParams,
-    actions: FormikHelpers<CreateParams>,
+
+  const handleClick = () => {
+    router.back();
+  };
+
+  const handleChange = (
+    event: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >,
   ) => {
-    const valuesTrimmer = {
-      tag: values.tag,
-      content: values.content.trim(),
-      title: values.title.trim(),
+    setDraft({
+      ...draft,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (formData: FormData) => {
+    const content = formData.get('content');
+    const title = formData.get('title');
+    const tag = formData.get('tag');
+
+    if (
+      typeof content !== 'string' ||
+      typeof title !== 'string' ||
+      typeof tag !== 'string'
+    ) {
+      return;
+    }
+
+    const newNote: CreateParams = {
+      title: title.trim(),
+      tag,
+      content: content.trim(),
     };
 
-    noteMutation.mutate(valuesTrimmer);
-
-    actions.resetForm();
+    try {
+      await validationSchema.validate(newNote, { abortEarly: false });
+      createNoteMutation.mutate(newNote);
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        error.errors.forEach((message) =>
+          toast.error(message, { duration: 7000 }),
+        );
+      }
+    }
   };
 
   return (
-    <Formik
-      initialValues={{ title: '', content: '', tag: '' }}
-      onSubmit={formValues}
-      validationSchema={newValidationSchema}
-    >
-      <Form className={css.form}>
+    <>
+      <Toaster position="top-right" />
+      <form className={css.form} action={handleSubmit}>
         <div className={css.formGroup}>
-          <label htmlFor="title">Title</label>
-          <Field id="title" type="text" name="title" className={css.input} />
-          <ErrorMessage name="title" className={css.error} />
+          <label htmlFor="title">Title </label>
+          <input
+            id="title"
+            type="text"
+            name="title"
+            className={css.input}
+            value={draft.title}
+            onChange={handleChange}
+          />
         </div>
 
         <div className={css.formGroup}>
-          <label htmlFor="content">Content</label>
-          <Field
-            as="textarea"
+          <label htmlFor="content">Content </label>
+          <textarea
             id="content"
             name="content"
             rows={8}
             className={css.textarea}
-          />
-          <ErrorMessage name="content" className={css.error} />
+            value={draft.content}
+            onChange={handleChange}
+          ></textarea>
         </div>
 
         <div className={css.formGroup}>
-          <label htmlFor="tag">Tag</label>
-          <Field as="select" id="tag" name="tag" className={css.select}>
+          <label htmlFor="tag">Tag </label>
+          <select
+            id="tag"
+            name="tag"
+            className={css.select}
+            onChange={handleChange}
+            value={draft.tag}
+          >
             <option value="Todo">Todo</option>
             <option value="Work">Work</option>
             <option value="Personal">Personal</option>
             <option value="Meeting">Meeting</option>
             <option value="Shopping">Shopping</option>
-          </Field>
-          <ErrorMessage name="tag" className={css.error} />
+          </select>
         </div>
 
         <div className={css.actions}>
-          <button type="button" className={css.cancelButton} onClick={onClose}>
+          <button
+            type="button"
+            className={css.cancelButton}
+            onClick={handleClick}
+          >
             Cancel
           </button>
           <button type="submit" className={css.submitButton}>
             Create note
           </button>
         </div>
-      </Form>
-    </Formik>
+      </form>
+    </>
   );
 }
+// const queryClient = useQueryClient();
+// const router = useRouter();
+// const { draft, setDraft, clearDraft } = useNoteDraftStore();
+
+// const handleChange = (
+//   event: React.ChangeEvent<
+//     HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+//   >,
+// ) => {
+//   setDraft({
+//     ...draft,
+//     [event.target.name]: event.target.value,
+//   });
+// };
+// const { mutate } = useMutation({
+//   mutationFn: createNote,
+//   // 5. При успішному створенні нотатки очищуємо чернетку
+//   onSuccess: () => {
+//     clearDraft();
+//     router.push('/notes/filter/all');
+//   },
+// });
+
+// const handleSubmit = (formData: FormData) => {
+//   const values = Object.fromEntries(formData) as NewNoteData;
+//   mutate(values);
+// };
+
+// const handleCancel = () => router.push('/notes/filter/all');
+
+// const noteMutation = useMutation({
+//   mutationFn: (newNote: CreateParams) => createNote(newNote),
+//   onSuccess: () => {
+//     queryClient.invalidateQueries({ queryKey: ['notes'] });
+//     onClose();
+//     toast.success('Note added successfully!');
+//   },
+//   onError: () => {
+//     toast.error('Failed to add note');
+//   },
+// });
+// const formValues = (
+//   values: CreateParams,
+//   actions: FormikHelpers<CreateParams>,
+// ) => {
+//   const valuesTrimmer = {
+//     tag: values.tag,
+//     content: values.content.trim(),
+//     title: values.title.trim(),
+//   };
+
+//   noteMutation.mutate(valuesTrimmer);
+
+//   actions.resetForm();
+// };
+
+//   return (
+//     <>
+//       <Toaster position="top-right" />
+//       <form className={css.form}>
+//         <div className={css.formGroup}>
+//           <label htmlFor="title">Title</label>
+//           <input id="title" type="text" name="title" className={css.input} />
+//         </div>
+
+//         <div className={css.formGroup}>
+//           <label htmlFor="content">Content</label>
+//           <input
+//             id="content"
+//             type="text"
+//             name="content"
+//             className={css.textarea}
+//           />
+//         </div>
+
+//         <div className={css.formGroup}>
+//           <label htmlFor="tag">Tag</label>
+//           <select id="tag" name="tag" className={css.select}>
+//             <option value="Todo">Todo</option>
+//             <option value="Work">Work</option>
+//             <option value="Personal">Personal</option>
+//             <option value="Meeting">Meeting</option>
+//             <option value="Shopping">Shopping</option>
+//           </select>
+//         </div>
+
+//         <div className={css.actions}>
+//           <button type="button" className={css.cancelButton} onClick={onClose}>
+//             Cancel
+//           </button>
+//           <button type="submit" className={css.submitButton}>
+//             Create note
+//           </button>
+//         </div>
+//       </form>
+//     </>
+//   );
+// }
